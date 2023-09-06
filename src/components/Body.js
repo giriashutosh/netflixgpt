fd746b461262e41b99251b3cadcc0d884e8cc500
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-09-06 15:59:53 +0530

    added the store and authstatechange

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Login from './Login'
 import Browse from './Browse'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../utils/firebase'
+import { useDispatch } from 'react-redux'
+import { addUser, removeUser } from '../store/UserSlice'
+
 const appRouter = createBrowserRouter([
     {
         path: '/',
@@ -11,7 +16,27 @@ const appRouter = createBrowserRouter([
         element: <Browse/>
     }
 ])
+
 const Body = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const {displayName, email, uid } = user;
+        console.log(user)
+        dispatch(addUser({uid, email, displayName}))
+        //const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser())
+      }
+    });
+    
+  }, [])
+  
   return (
     <div>
        <RouterProvider router={appRouter}/>
