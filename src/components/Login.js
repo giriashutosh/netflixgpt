75e387ca1040ef8aa99ea5b759eb5576459550fb
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-09-06 12:20:18 +0530

    added the authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Header from './Header'
-
+import { checkValidation } from '../utils/validation'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../utils/firebase'
 const Login = () => {
     const [isSignIn, setIsSignIn] = useState(true)
-    
-    const toggleSignIn = () =>{
+    const [errMssg, setErrorMssg] = useState(null)
+    const email = useRef(null)
+    const password = useRef(null)
+    const name = useRef(null)
+
+    const validationHandler = () => {
+        //console.log(email);
+        const mssg = checkValidation(email.current.value, password.current.value);
+        setErrorMssg(mssg)
+        if (mssg) return;
+
+        if (!isSignIn) {
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user)
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode + errorMessage)
+                });
+        } else {
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user)
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode + errorMessage)
+                });
+        }
+
+    }
+    const toggleSignIn = () => {
         setIsSignIn(!isSignIn)
     }
 
@@ -18,28 +57,33 @@ const Login = () => {
                     alt=''
                 />
             </div>
-            <form className='w-4/12 p-12 absolute bg-black my-36 mx-auto left-0 right-0 opacity-80 rounded-lg'>
+            <form onSubmit={(e) => e.preventDefault()} className='w-4/12 p-12 absolute bg-black my-36 mx-auto left-0 right-0 opacity-80 rounded-lg' noValidate>
                 <h1 className='m-4 text-white font-bold text-3xl'>{isSignIn ? "Sign In" : "Sign Up"}</h1>
                 {!isSignIn &&
-                 <input
-                 type="text"
-                 placeholder='Full Name'
-                 className='w-full p-2 m-4 bg-slate-950 '
-                 
-                 />
+                    <input
+                        ref={name}
+                        type="text"
+                        placeholder='Full Name'
+                        className='w-full p-2 m-4 bg-slate-950 text-white'
+
+                    />
                 }
                 <input
-                type="email"
-                placeholder='Enter your email or Phone Number'
-                className='w-full p-2 m-4 bg-slate-950'
+                    ref={email}
+                    type="email"
+                    formNoValidate
+                    placeholder='Enter your email or Phone Number'
+                    className='w-full p-2 m-4 bg-slate-950 text-white'
                 />
                 <input
-                type='password'
-                placeholder='Enter your password'
-                className='w-full p-2 m-4 bg-slate-950'
+                    ref={password}
+                    type='password'
+                    placeholder='Enter your password'
+                    className='w-full p-2 m-4 bg-slate-950 text-white'
                 />
-                <button className='w-full p-2 m-4 text-white bg-red-700 rounded-lg'>{isSignIn ? "Sign In": "Sign Up"}</button>
-                <p className='text-white m-4 cursor-pointer' onClick={toggleSignIn}>{isSignIn? "New to Netflix? Sign up now.":"Already a User? Sign In now."}</p>
+                <button className='w-full p-2 m-4 text-white bg-red-700 rounded-lg' onClick={validationHandler}>{isSignIn ? "Sign In" : "Sign Up"}</button>
+                <p className='py-2 m-4 text-red-600 font-bold text-lg'>{errMssg}</p>
+                <p className='text-white m-4 cursor-pointer' onClick={toggleSignIn}>{isSignIn ? "New to Netflix? Sign up now." : "Already a User? Sign In now."}</p>
             </form>
         </div>
 
---
+++ src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdPvnW66lRb0Qscqe02DJApV3Y8UEB5Zw",
+  authDomain: "netflixgpt-8bcd1.firebaseapp.com",
+  projectId: "netflixgpt-8bcd1",
+  storageBucket: "netflixgpt-8bcd1.appspot.com",
+  messagingSenderId: "175881118460",
+  appId: "1:175881118460:web:474aabbdcbd77d5a39ad79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
