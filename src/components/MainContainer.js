2f314c774c29f62ef378876ecc2e8067b165abc4
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-09-09 13:07:45 +0530

    add mainContainer, customHook

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const MainContainer = () => {
+    const movies = useSelector(store => store.movies?.nowPlayingMovies)
+    if (movies === null) return null;
+
+    const mainMovie = movies[0];
+    
   return (
     <div>
       MainContainer
